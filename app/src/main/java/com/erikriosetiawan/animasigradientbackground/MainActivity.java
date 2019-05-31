2c4d8afd10b4c0eb2ae9b67062a176f24d18e7e0
erik1997 <erikriosetiawan49@gmail.com>
2019-05-31 09:12:24 +0700

    Initial commit

---
+++ app/src/main/java/com/erikriosetiawan/animasigradientbackground/MainActivity.java
@@ -0,0 +1,25 @@
+package com.erikriosetiawan.animasigradientbackground;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout constraintLayout;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.root_layout);
+
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
